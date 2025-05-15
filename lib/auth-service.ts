c4619eadf2bb438009b0aaca895202c090021fcf
ravirajzaveri <38056381+ravirajzaveri@users.noreply.github.com>
2025-05-15 20:59:58 +0530

    Update auth-service.ts

--- lib/auth-service.ts
+++ lib/auth-service.ts
@@ -27,3 +27,26 @@ export const getSelf = async () => {
 
   return user;
 };
+export const getSelfByUsername = async (username: string) => {
+  const self = await currentUser();
+
+  if (!self || !self.username) {
+    throw new Error("Unauthorized");
+  }
+
+  const user = await db.user.findUnique({
+    where: {
+      username,
+    },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  if (self.username !== username) {
+    throw new Error("Unauthorized");
+  }
+
+  return user;
+};
